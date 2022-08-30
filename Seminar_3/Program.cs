e9f15ec2f3121609513037078db5799014719e83
Denis <disa.92@list.ru>
2022-08-31 00:25:09 +0700

    the task is resolved

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,16 @@
+﻿int FindQuadrant (double x, double y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+
+Console.Write("Введите координаты Х: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты Y: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+
+int quadrant = FindQuadrant(xA, yA );
+Console.WriteLine($"Number of quadrant is {quadrant}");
